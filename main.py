35e54a4726fb179161a559c983d6e6f51677c74f
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2022-09-11 22:42:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ while True:
     country  =random.choice(['CN','BR','UK','US'])
     max      =random.choice('1234')
     pay      =random.choice(['CC','DC'])
+    dot      =random.choice(['.',''])
     #emmanuelsouza53@gmail.com:eusoudos3 | SUBSCRIBED | Country: BR | Free trial: false | Current plan: Padr u00E3o | Has UHD: false | Max streams: 2 | Payment method: CC
-    f.write(f'{first}.{last}{birthday1}{birthday2}{birthday3}{birthday4}@{domain} | SUBSCRIBED | Country:{country} | Free trial: false | Current plan: Padr u00E3o | Has UHD: false | Max streams: {max} | Payment method: {pay}\n')
+    f.write(f'{first}{dot}{last}{birthday1}{birthday2}{birthday3}{birthday4}@{domain} | SUBSCRIBED | Country:{country} | Free trial: false | Current plan: Padr u00E3o | Has UHD: false | Max streams: {max} | Payment method: {pay}\n')
     q+=1
