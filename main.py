2678443b1916cb7f0b31fe417765b29ad83be362
orange a <81461409+minato37103710@users.noreply.github.com>
2022-09-11 12:47:15 +0000

    メアドげん作成

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import names
+import random
 
+first=names.get_first_name()
+last =names.get_last_name()
+birthday1=random.choice('1')
+birthday2=random.choice('012')
+birthday3=random.choice('12')
+birthday4=random.choice('0123456789')
+
+print(f'{first}.{last}{birthday1}{birthday2}{birthday3}{birthday4}@gmail.com')
